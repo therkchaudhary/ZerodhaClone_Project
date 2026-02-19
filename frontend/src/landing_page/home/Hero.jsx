476e41f6c1075b484b9cb2ec86d96e363aeaeff5
Rahul Chaudhary <rkchauhary9354@gmail.com>
2026-02-19 20:11:41 +0530

    mobile responsive

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -5,7 +5,8 @@ function Hero() {
     return (
         <div className='container p-3 p-md-5 mb-5'>
             <div className='row text-center'>
-                <img src="media/homeHero.png" alt="Hero Image" className='mb-5' />
+                {/* Added img-fluid for mobile responsiveness */}
+                <img src="media/homeHero.png" alt="Hero Image" className='mb-5 img-fluid' />
                 <h1 className='mt-5'>Invest in everthing</h1>
                 <p>
                     Online platform to invest in stocks, derivatives, mutual, and more
